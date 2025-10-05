2f70e26c2b76e19f6448e756a47a9de146e97089
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-10-05 01:10:16 -0400

    feat: added buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,23 +7,38 @@
 
 <main>
 	<div class="relative h-screen w-full overflow-hidden bg-sky-background">
-		<img
-			src={SwampHacksTitle}
-			alt="SwampHacks Title"
-			class="absolute top-1/2 left-1/2 w-4/5 -translate-x-1/2
-			   -translate-y-1/2 sm:w-fit md:top-1/3 md:left-2/5 md:h-1/6 md:translate-x-0 md:translate-y-0 select-none pointer-events-none"
-		/>
+		<!-- Title + Button Container -->
+		<div
+			class="absolute top-1/2 left-1/2 flex
+				   -translate-x-1/2 -translate-y-1/2 flex-col items-center
+				   gap-4 space-y-6 select-none md:top-[30%] md:left-2/5 md:translate-x-0 md:translate-y-0 w-full sm:w-fit"
+		>
+			<img
+				src={SwampHacksTitle}
+				alt="SwampHacks Title"
+				class="pointer-events-none w-4/5 sm:w-fit md:h-1/6"
+			/>
 
+			<!-- Button -->
+			<div class="flex w-full flex-col items-center justify-center gap-4">
+				<button class="w-1/2 cursor-pointer bg-button-primary py-4 rounded-md"> Register To Hack </button>
+
+				<button class="w-1/2 cursor-pointer bg-button-primary py-4 rounded-md"> Apply to Mentor </button>
+			</div>
+		</div>
+
+		<!-- Century Tower -->
 		<img
 			src={CenturyTower}
 			alt="Century Tower"
-			class="absolute bottom-0 left-[16%] h-[90%] w-auto max-w-full object-contain select-none pointer-events-none"
+			class="pointer-events-none absolute bottom-0 left-[16%] h-[30%] sm:h-[90%] w-auto max-w-full object-contain select-none"
 		/>
 
+		<!-- Bush Wall -->
 		<img
 			src={BushWall}
 			alt="Bush wall"
-			class="absolute bottom-[-10px] left-0 h-1/8 w-full object-cover select-none pointer-events-none"
+			class="pointer-events-none absolute bottom-[-10px] left-0 h-[7%] sm:h-1/8 w-full object-cover select-none"
 		/>
 	</div>
 
