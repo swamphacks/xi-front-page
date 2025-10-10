27f4297deb237fa5917e8e8828e758b77a038294
AlexanderWangY <alexander.yisu.wang@outlook.com>
2025-10-10 17:29:22 -0400

    wopw this literally looks so bad rn

---
+++ src/lib/components/Road.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Road from '$lib/assets/Road.svg';
+  </script>
+  
+  <img
+    src={Road}
+    alt="Road"
+    class="select-none scale-[18] md:scale-[4] origin-top"
+    draggable="false"
+  />
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <script>
-	import CenturyTower from '$lib/components/Landing/CenturyTower.svelte';
 	import Landing from '$lib/components/Landing/Landing.svelte';
+	import Road from '$lib/components/Road.svelte';
 </script>
 
 <main>
 	<Landing />
-	<div class="h-screen w-full bg-grass-background"></div>
-</main>
+	<div class="relative h-[400vh] w-full bg-grass-background">
+		<div class="absolute top-[-20px] left-[90%] -translate-x-1/2 md:top-[-50px] md:left-3/4">
+			<Road />
+		</div>
 
-<!-- <p class="title-text text-center m-4 sm:m-6 text-5xl sm:text-7xl 2xl:text-8xl font-bold tracking-wide leading-14 md:leading-20 2xl:leading-32">SwampHacks XI</p> -->
-<!-- 
-<ScrollablePath /> -->
+		<!-- Content goes here -->
+	</div>
+
+	<div
+		class="relative h-screen w-full bg-center bg-grass-background"
+		style="background-image: url('/src/lib/assets/Lake.png');"
+	>
+		<!-- Optional: overlay content -->
+	</div>
+</main>
