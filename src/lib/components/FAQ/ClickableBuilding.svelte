78d773ba5689c4d8f3e2bf419863d1c3ad56680d
h1divp <71522316+h1divp@users.noreply.github.com>
2025-10-15 22:46:03 -0400

    fix: mobile scaling issue

--- src/lib/components/FAQ/ClickableBuilding.svelte
+++ src/lib/components/FAQ/ClickableBuilding.svelte
@@ -1,25 +1,25 @@
-
 <script lang="ts">
-    export let src = "$lib/assets/Hippodrome.png";
-    export let alt = "Button Image";
-    export let title = "Title"
-    export let onClick;
+	export let src = '$lib/assets/Hippodrome.png';
+	export let alt = 'Button Image';
+	export let title = 'Title';
+	export let onClick;
 </script>
 
-
-
-<div class="flex w-auto items-center justify-center max-w-xl">
-    <div
-        class="flex justify-center size-full transform gap-2 border-2 border-dashed bg-white p-2 shadow-2xl transition-transform duration-300 group hover:-rotate-1"
-        onclick={onClick}
-         onkeydown={(event) => {}}
-         role="button"
-         tabindex="0" 
-    >
-        
-        <div class="relative flex flex-col h-full w-full gap-4 bg-[#E8DCC4] p-6">
-            <h3 class="font-beachday text-gray-800 text-4xl text-center cursor-default">{title}</h3>
-            <img src={src} alt={alt} style="display: block; width: 100%; height: auto;" class="opacity-90 group-hover:opacity-100 justify-center" />
-        </div>
-    </div>
+<div class="m-4 flex w-max items-center justify-center lg:w-2/5">
+	<div
+		class="group flex size-full transform justify-center gap-2 border-2 border-dashed bg-white p-2 shadow-2xl transition-transform duration-300 hover:scale-105 hover:-rotate-1"
+		onclick={onClick}
+		onkeydown={(event) => {}}
+		role="button"
+		tabindex="0"
+	>
+		<div class="relative flex h-full w-full flex-col gap-4 bg-[#E8DCC4] p-6">
+			<h3 class="cursor-default text-center font-beachday text-4xl text-gray-800">{title}</h3>
+			<img
+				{src}
+				{alt}
+				class="block h-auto w-max justify-center object-contain opacity-90 group-hover:opacity-100"
+			/>
+		</div>
+	</div>
 </div>
