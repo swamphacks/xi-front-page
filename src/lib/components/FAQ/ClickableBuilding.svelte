6cf21af7c5f11b8112c4158770ad27e61cd7dc90
h1divp <71522316+h1divp@users.noreply.github.com>
2025-10-16 01:08:21 -0400

    fix: cursor set to pointer

--- src/lib/components/FAQ/ClickableBuilding.svelte
+++ src/lib/components/FAQ/ClickableBuilding.svelte
@@ -5,7 +5,7 @@
 	export let onClick;
 </script>
 
-<div class="m-4 flex w-max items-center justify-center lg:w-2/5">
+<div class="m-4 flex w-max cursor-pointer items-center justify-center lg:w-2/5">
 	<div
 		class="group flex size-full transform justify-center gap-2 border-2 border-dashed bg-white p-2 shadow-2xl transition-transform duration-300 hover:scale-105 hover:-rotate-1"
 		onclick={onClick}
@@ -14,7 +14,7 @@
 		tabindex="0"
 	>
 		<div class="relative flex h-full w-full flex-col gap-4 bg-[#E8DCC4] p-6">
-			<h3 class="cursor-default text-center font-beachday text-4xl text-gray-800">{title}</h3>
+			<h3 class="text-center font-beachday text-4xl text-gray-800">{title}</h3>
 			<img
 				{src}
 				{alt}
