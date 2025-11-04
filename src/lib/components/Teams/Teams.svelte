6f0d6c9a3a680aefecd268581e5451bf3117a460
Stanley Ke <kestanley101@gmail.com>
2025-11-03 19:06:42 -0500

    fix: remove sign and formatting of stdium on mobile

--- src/lib/components/Teams/Teams.svelte
+++ src/lib/components/Teams/Teams.svelte
@@ -1,10 +1,21 @@
 <script>
-	import Stadium from '$lib/assets/stadium.svg';
-	import TeamMap from './TeamMap.svelte';
+    import Stadium from '$lib/assets/stadium.svg';
+    import TeamMap from './TeamMap.svelte';
 </script>
 
-<div class="flex size-full flex-col justify-center pb-10">
-	<h2 class="mb-20 text-center font-beachday text-5xl text-white md:text-7xl">Meet the team!</h2>
-	<!-- Team Members Here -->
-	<TeamMap />
+<div class="relative flex flex-col pb-0">
+    <h2 class="relative z-10 mb-10 pt-4 text-center font-beachday text-5xl text-white md:text-7xl">Meet the team!</h2>
+    <!-- Team Members Here -->
+    <div class="relative team-stadium-container min-h-[200px]" style="height: calc(120vw * 4828 / 8346);">
+        <TeamMap />
+    </div>
 </div>
+
+<style>
+    @media (min-width: 768px) {
+        .team-stadium-container {
+            height: calc(105vw * 4828 / 8346) !important;
+            max-height: 1500px;
+        }
+    }
+</style>
